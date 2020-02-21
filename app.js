6c9941f2b85acb5108061b52558ebe81890c55ff
myguddy <seungil.han@gmail.com>
2020-02-21 20:20:16 +0900

    prevent 304

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api', contentRouter);
 
+app.use('/api', contentRouter);
+app.disable('etag')
 module.exports = app;
